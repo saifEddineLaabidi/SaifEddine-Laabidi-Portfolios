c5f6737f06afbdaf1453c7b32f200c5e95a1e433
Saif Eddine LAABIDI <slaabidi@sofrecom.fr>
2025-08-11 18:30:20 +0100

    Fix: Ajoute le préfixe de chemin aux certifications et au CV

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -1,15 +1,17 @@
+const prefix = process.env.NODE_ENV === 'production' ? '/SaifEddine-Laabidi-Portfolios' : '';
+
 export const certifications = [
   {
     name: "Red Hat Certified System Administrator (RHCSA)",
-    image: "/skills/rhcsa.png",
+    image: `${prefix}/skills/rhcsa.png`,
   },
   {
     name: "Red Hat Certified Engineer (RHCE)",
-    image: "/skills/rhce.png",
+    image: `${prefix}/skills/rhce.png`,
   },
   {
     name: "SAFe® 6 Practitioner",
-    image: "/skills/safe.png",
+    image: `${prefix}/skills/safe.png`,
   },
 ];
 
